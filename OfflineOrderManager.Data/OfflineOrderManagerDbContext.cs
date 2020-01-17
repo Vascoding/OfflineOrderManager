b7bdcbb64a5a7eb7cb4f6cb6c27abf5b3d210512
Vasil Valchanov <vasil.va@sbtech.com>
2020-01-17 20:13:37 +0200

    Initial commit

---
+++ OfflineOrderManager.Data/OfflineOrderManagerDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace OfflineOrderManager.Data
+{
+    public class OfflineOrderManagerDbContext : DbContext
+    {
+        public OfflineOrderManagerDbContext(DbContextOptions<OfflineOrderManagerDbContext> options)
+            : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
